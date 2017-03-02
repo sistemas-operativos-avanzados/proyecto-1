175253c80072470e2587dc9972daa6455e16a194
Raquel Elizondo Barrios <rackelelizondo@gmail.com>
2017-03-02 09:38:03 -0600

    Manejo de ciclos y recepcion de respuesta del server corregidos

--- client/client.c
+++ client/client.c
@@ -11,7 +11,7 @@
 
 
 char *host, *filename, *portno;
-int d_sock;
+int d_sock, ncycles = 0;
 
 void error(char* msg) {
     fprintf(stderr, "%s: %s\n", msg, strerror(errno));
@@ -46,74 +46,60 @@ int open_socket(char* host, char* port) {
 }
 
 static void *make_request() {
-    char buf[255];
-    sprintf(buf, "GET /%s HTTP/1.1\r\nHost:%s\r\n\r\n", filename, host);
-    send(d_sock, buf, strlen(buf), 0);
-    char rec[256];
-    int bytesRcvd;
-    long unsigned bytes;
-
+  int cycles = 0;
+  while(cycles < ncycles){
     do{
-      	bytesRcvd = recv(d_sock, rec, 256, 0);
-	if (bytesRcvd < 0)
-            error("Can't read from server");
+       char buf[1023];
+       sprintf(buf, "GET /%s HTTP/1.1\r\nHost:%s\r\n\r\n", filename, host);
+    
+       if((send(d_sock, buf, strlen(buf), 0)) == 0)
+         error("Error to send");
 
-        if (bytesRcvd == 0)
+       char rec[1024];
+       ssize_t bytesRcvd;
+       if ((bytesRcvd = recv(d_sock, rec, 1024, 0)) == 0)
             break;
 
+        bytesRcvd = read(d_sock, rec, 1024);
         rec[bytesRcvd] = '\0';
-        bytes = bytes + sizeof(rec);
-	    //Print received data
         //printf("%s", rec);
 
-    //}while (1); //Esta deberia ser la condicion correcta
-    
-    }while (bytesRcvd == 256);//Temporal workaround: when the server send least than 256 bytes the client keeps listening, server doesn't send any data and program never closes the connection
- 
+    }while (1); 
+  cycles++;
+}
+ close(d_sock);
  return (NULL);
 }
 
 int main(int argc, char* argv[]) {
     
     if(argc != 6){
-        //ei. ./client www.smartercommerce.net 80 resources/webinars 3 2
+        //ei. ./client proveinca.com 80 fs_files/user_img/Composicion%20PEX-AL-PEX.jpg 2 120
+
         printf("\n Usage: %s <host> <port> <filename> <n-threads> <n-cycles>", argv[0]);
         return 1;
     }
-    int nthreads = 0, ncycles = 0;
+    int nthreads = 0;
     
     host = argv[1];
     portno = argv[2];
     filename = argv[3];
     nthreads = atoi(argv[4]);
     ncycles = atoi(argv[5]);
-    d_sock = open_socket(host, portno);
+    int i = 1, y = 1;
+    pthread_t tid;
 
-    int i = 0, y = 0;
-    int err;
-
-    while(i < nthreads)
+    while(i <= nthreads)
     {
-        
-	y = 0;
-	do{
-        //Si el numero de ciclos es 0, igualmente, crea el thread y lo ejecuta una vez
-        pthread_t tid;
-	    err = pthread_create(&(tid), NULL, &make_request, NULL);
-        
-	   if (err != 0)
-	    printf("\ncan't create thread :[%s]", strerror(err));
-	   else{
-	    int threadno = i + 1;
-        int cycleno = y + 1;
-	    printf("Thread %i del ciclo %i fue creado correctamente\n", threadno, cycleno);
-	    pthread_join(tid, NULL);
-	   } 
-		y++;
-	    }while(y < ncycles);
+        d_sock = open_socket(host, portno);
+	int err = pthread_create(&(tid), NULL, &make_request, NULL);
+        printf("Thread id %lu fue creado correctamente\n", pthread_self());
+	if (err != 0)
+	    error("Can't create de thread");
+	 else
+         pthread_join(tid, NULL);
      i++;
      }
 		
-    close(d_sock);
     return 0;
 }
